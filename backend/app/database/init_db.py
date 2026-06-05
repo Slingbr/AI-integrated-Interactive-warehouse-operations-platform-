b183eac2a9ea7e0d0069a95516157d2f3919428d
eboateng <Eboatenggraham@gmail.com>
2026-06-05 01:15:53 +0100

    working test for table creation in database

--- backend/app/database/init_db.py
+++ backend/app/database/init_db.py
@@ -3,4 +3,5 @@ from app.models.product import Product
 
 Base.metadata.create_all(bind=engine)
 
-print("Database tables created.")
+print("Database table creation test successfull.")
+print("Products created")
