bafafc293c9866aee96e3c1e32fab3af4b734a1e
eboateng <Eboatenggraham@gmail.com>
2026-06-13 03:23:23 +0100

    mini fast api app with DB session

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.routes.product_routes import router as product_router
 
 app = FastAPI()
 
+app.include_router(product_router)
 
 @app.get("/")
 def root():
---
+++ backend/app/routes/product_routes.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.session import get_db
+from app.models.product import Product
+from app.schemas.product import ProductCreate, ProductResponse
+
+router = APIRouter()
+
+
+@router.post("/products", response_model=ProductResponse)
+def create_product(
+    product: ProductCreate,
+    db: Session = Depends(get_db)
+):
+    new_product = Product(
+        sku=product.sku,
+        name=product.name,
+        quantity=product.quantity,
+        aisle=product.aisle,
+        shelf=product.shelf
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return new_product
