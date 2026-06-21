3a9f8776fa4fbf4d431937a91b0f217d2201d258
eboateng <Eboatenggraham@gmail.com>
2026-06-21 02:32:11 +0100

    creating func for getting product by id

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.session import get_db
@@ -31,3 +31,15 @@ def create_product(
 def get_products(db: Session = Depends(get_db)):
     products = db.query(Product).all()
     return products
+
+@router.get("/products/{product_id}", response_model=ProductResponse)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+
+    if product is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+
+    return product
