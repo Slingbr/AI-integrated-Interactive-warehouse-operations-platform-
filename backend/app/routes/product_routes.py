3fd4b635ac78459c9a259c53c0382095846011f8
eboateng <Eboatenggraham@gmail.com>
2026-06-28 00:48:36 +0100

    simplified create call

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database.session import get_db
 from app.models.product import Product
 from app.schemas.product import ProductCreate, ProductResponse
-
+from app.services import product_service
 router = APIRouter()
 
 
@@ -13,19 +13,7 @@ def create_product(
     product: ProductCreate,
     db: Session = Depends(get_db)
 ):
-    new_product = Product(
-        sku=product.sku,
-        name=product.name,
-        quantity=product.quantity,
-        aisle=product.aisle,
-        shelf=product.shelf
-    )
-
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-
-    return new_product
+     return product_service.create_product(db, product)
 
 @router.get("/products", response_model=list[ProductResponse])
 def get_products(db: Session = Depends(get_db)):
