271fd59fa656b3faae58041dbdc3e4753382362f
eboateng <Eboatenggraham@gmail.com>
2026-08-13 19:46:42 +0100

    Changed some imports fixing an error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes.product_routes import router as product_router
-
+from app.models import product, warehouse_location
 app = FastAPI()
 
 app.include_router(product_router)
