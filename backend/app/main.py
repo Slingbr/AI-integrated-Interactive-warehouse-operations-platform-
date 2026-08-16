975583e455d6e25ab47c0597c068e17ccf7a84de
eboateng <Eboatenggraham@gmail.com>
2026-08-16 01:44:47 +0100

    updated main to include new routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routes.product_routes import router as product_router
 from app.models import product, warehouse_location
+from app.routes.worker_routes import router as worker_router
 app = FastAPI()
 
 app.include_router(product_router)
+app.include_router(worker_router)
 
 @app.get("/")
 def root():
