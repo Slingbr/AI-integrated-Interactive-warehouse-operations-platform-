68e8b84430d1187db2ff86a8bba59dda18de74bc
eboateng <Eboatenggraham@gmail.com>
2026-08-24 02:55:51 +0100

    extra imprts

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ from app.routes.product_routes import router as product_router
 from app.models import product, warehouse_location
 from app.routes.worker_routes import router as worker_router
 from app.routes.order_routes import router as order_router
+from app.models.order import Order
+from app.models.order_item import OrderItem
 app = FastAPI()
 
 app.include_router(product_router)
