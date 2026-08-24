c5f4959be106e269b4e868b4b68d292b16a68214
eboateng <Eboatenggraham@gmail.com>
2026-08-24 02:38:08 +0100

    forgot to add imports to init

--- backend/app/database/init_db.py
+++ backend/app/database/init_db.py
@@ -1,6 +1,8 @@
 from app.database.connection import Base, engine
 from app.models.product import Product
 from app.models.warehouse_location import WarehouseLocation
+from app.models.order_item import OrderItem
+from app.models.order import Order
 
 Base.metadata.create_all(bind=engine)
 
